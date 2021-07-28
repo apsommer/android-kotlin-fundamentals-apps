df47d48d72b33383283384add24e7e46fc3b0cba
Drew Sommer <andrew.sommer@tiaa.org>
2021-07-28 17:59:01 -0500

    feat: Transform livedata emissions using map

--- GuessTheWordDataBinding/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWordDataBinding/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,9 +17,11 @@
 package com.example.android.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /**
@@ -42,10 +44,10 @@ class GameViewModel : ViewModel() {
     val eventGameFinish: LiveData<Boolean>
         get() = _eventGameFinish
 
-    // countdown time
+    // countdown timer
     private val _currentTime = MutableLiveData<Long>()
-    val currentTime: LiveData<Long>
-        get() = _currentTime
+    val currentTime: LiveData<Long> get() = _currentTime
+    val currentTimeString = Transformations.map(currentTime) { time -> DateUtils.formatElapsedTime(time) } // executed on main thread, be careful
 
     // The list of words - the front of the list is the next _word to guess
     private lateinit var wordList: MutableList<String>
